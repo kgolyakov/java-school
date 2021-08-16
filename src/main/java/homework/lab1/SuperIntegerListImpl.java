df312ca7d9c52b00e100986d5351e53b61a7408e
k.golyakov <k.golyakov.1998@yandex.ru>
2021-08-16 22:45:17 +0300

    Lab 1 done Implemented singly linked list

--- src/main/java/homework/lab1/SuperIntegerListImpl.java
+++ src/main/java/homework/lab1/SuperIntegerListImpl.java
@@ -1,30 +1,88 @@
 package homework.lab1;
 
 public class SuperIntegerListImpl implements SuperIntegerList{
+    public class ListEntry {
+        int key;
+        ListEntry next;
+    }
 
+    ListEntry data = null;
 
     @Override
     public void add(int number) {
-
+        if (data == null) {
+            data = new ListEntry();
+            data.key = number;
+            data.next = null;
+        } else {
+            ListEntry tmp = data;
+            while (tmp.next != null) {
+                tmp = tmp.next;
+            }
+            tmp.next = new ListEntry();
+            tmp.next.key = number;
+            tmp.next.next = null;
+        }
     }
 
     @Override
     public void removeByIndex(int index) {
-
+        if (index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            data = data.next;
+        } else {
+            ListEntry tmp = data;
+            for (int i = 0; i < index - 1; i++) {
+                tmp = tmp.next;
+                if (tmp == null || tmp.next == null) {
+                    throw new IndexOutOfBoundsException();
+                }
+            }
+            tmp.next = tmp.next.next;
+        }
     }
 
     @Override
     public void removeByValue(int value) {
-
+        while (data != null && data.key == value) {
+            data = data.next;
+        }
+        ListEntry tmp = data;
+        while (tmp != null) {
+            while (tmp.next != null && tmp.next.key == value) {
+                tmp.next = tmp.next.next;
+            }
+            tmp = tmp.next;
+        }
     }
 
     @Override
     public int get(int index) {
-        return 0;
+        if (index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        ListEntry tmp = data;
+        for (int i = 0; i < index; i++) {
+            if (tmp == null) {
+                throw new IndexOutOfBoundsException();
+            }
+            tmp = tmp.next;
+        }
+        if (tmp == null) {
+            throw new IndexOutOfBoundsException();
+        }
+        return tmp.key;
     }
 
     @Override
     public void printAll() {
-
+        ListEntry tmp = data;
+        while (tmp != null) {
+            System.out.print("" + tmp.key + " ");
+            tmp = tmp.next;
+        }
+        System.out.print("\n");
     }
 }
---
+++ src/test/java/SuperIntegerListImplTest.java
@@ -0,0 +1,77 @@
+import homework.lab1.SuperIntegerListImpl;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class SuperIntegerListImplTest {
+
+    @Test
+    public void addGetElements() {
+        SuperIntegerListImpl superIntegerList = new SuperIntegerListImpl();
+        assertThrows(IndexOutOfBoundsException.class, () -> superIntegerList.get(0), "List is empty");
+        superIntegerList.add(1);
+        assertEquals("First element is equal to 1", 1, superIntegerList.get(0));
+        superIntegerList.add(2);
+        assertEquals("Second element is equal to 2", 2, superIntegerList.get(1));
+        assertThrows(IndexOutOfBoundsException.class, () -> superIntegerList.get(2), "List doesn't have third argument");
+    }
+
+    @Test
+    public void removeElementsByValue() {
+        SuperIntegerListImpl superIntegerList = new SuperIntegerListImpl();
+        superIntegerList.add(1);
+        superIntegerList.add(1);
+        superIntegerList.add(2);
+        superIntegerList.add(1);
+        superIntegerList.add(2);
+        superIntegerList.removeByValue(1);
+        assertThrows(IndexOutOfBoundsException.class, () -> superIntegerList.get(2), "List doesn't have third argument after first removal");
+        assertEquals("Second element is equal to 2 after first removal", 2, superIntegerList.get(1));
+        superIntegerList.removeByValue(3);
+        assertThrows(IndexOutOfBoundsException.class, () -> superIntegerList.get(2), "List doesn't have third argument after second removal");
+        assertEquals("First element is equal to 2 after second removal", 2, superIntegerList.get(0));
+        assertEquals("Second element is equal to 2 after second removal", 2, superIntegerList.get(1));
+        superIntegerList.removeByValue(2);
+        assertThrows(IndexOutOfBoundsException.class, () -> superIntegerList.get(0), "List is empty");
+        superIntegerList.add(1);
+        assertEquals("First element is equal to 1 after third removal", 1, superIntegerList.get(0));
+    }
+
+    @Test
+    public void removeElementsByIndex() {
+        SuperIntegerListImpl superIntegerList = new SuperIntegerListImpl();
+        superIntegerList.add(1);
+        superIntegerList.add(2);
+        superIntegerList.add(3);
+        superIntegerList.add(4);
+        superIntegerList.add(5);
+        superIntegerList.removeByIndex(2);
+        assertEquals("Second element is equal to 2", 2, superIntegerList.get(1));
+        assertEquals("Third element is equal to 4", 4, superIntegerList.get(2));
+        assertThrows(IndexOutOfBoundsException.class, () -> superIntegerList.removeByIndex(4), "List doesn't have fifth argument");
+        assertThrows(IndexOutOfBoundsException.class, () -> superIntegerList.removeByIndex(-1), "List doesn't have negative arguments");
+        superIntegerList.removeByIndex(0);
+        assertEquals("First element is equal to 2", 2, superIntegerList.get(0));
+    }
+
+    @Test
+    public void checkOutput() {
+        PrintStream save_out = System.out;
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        SuperIntegerListImpl superIntegerList = new SuperIntegerListImpl();
+        superIntegerList.add(1);
+        superIntegerList.add(2);
+        superIntegerList.add(3);
+        superIntegerList.add(4);
+        superIntegerList.add(5);
+        superIntegerList.printAll();
+        assertEquals("1 2 3 4 5 \n", out.toString());
+        System.setOut(save_out);
+    }
+}
